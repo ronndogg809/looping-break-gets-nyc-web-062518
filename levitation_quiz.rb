faa49689a1d58f4016790be518226341a6b3741a
Ronald <ronaldcuello11@gmail.com>
2018-06-14 18:35:31 -0400

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,10 +1,10 @@
 
- # def levitation_quiz
+def levitation_quiz
 loop do
   puts "What is the spell that enacts levitation?"
 #what is going to be asked
    # get.chomp doesnt work in learn so commenting it out
-  answer = gets.chompb
+  answer = gets.chomp
 # will continue to push out above statement
   break if answer == "Wingardium Leviosa"
 #unless answer equals above ^^ then it would break the line
