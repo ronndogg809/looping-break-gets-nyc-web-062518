1881699a01e4c5e4ed463255ffb805698fa8d845
Ronald <ronaldcuello11@gmail.com>
2018-06-14 18:09:33 -0400

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,14 @@
 
 def levitation_quiz
-	#your code here
+loop do
+  puts "What is the spell that enacts levitation?"
+#what is going to be asked
+  answer = gets.chomp
+# will continue to push out above statement
+  break if answer == "Wingardium Leviosa"
+#unless answer equals above ^^ then it would break the line
 end
+puts "You passed the quiz!"
 
-
+#when line is broken this is what is returned.
+end
